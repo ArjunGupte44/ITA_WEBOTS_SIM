ec3f36a1051cc759dcb6ead533dba95f19267910
Arjun Gupte <guptea@purdue.edu>
2023-08-31 20:37:55 -0700

    finished initial implementation of table 2 LUT

--- webots_ros2_mavic/webots_ros2_mavic/OperatorHub.py
+++ webots_ros2_mavic/webots_ros2_mavic/OperatorHub.py
@@ -23,6 +23,7 @@ HUMAN_ATTRIBUTES_FILE = '/home/arjun/SMART-LAB-ITAP-WEBOTS/webots_ros2_mavic/res
 POI_ATTRIBUTES_FILE = '/home/arjun/SMART-LAB-ITAP-WEBOTS/webots_ros2_mavic/resource/poiAttributes'
 
 POIS_FILE = '/home/arjun/SMART-LAB-ITAP-WEBOTS/webots_ros2_mavic/resource/pois'
+NUM_AGENTS_FILE = '/home/arjun/SMART-LAB-ITAP-WEBOTS/webots_ros2_mavic/resource/numSimAgents'
 
 FIVE_MINS_IN_SECONDS = 300
 ONE_HOUR_IN_SECONDS = 3600
@@ -33,21 +34,34 @@ class OperatorHub(Node):
         super().__init__("OperatorHub")
         self.humanAttributes = self.getAgentAttributes('human', NUM_HUMAN_ATTRIBUTES)
         self.poiAttributes = self.getAgentAttributes('poi', NUM_POI_ATTRIBUTES)
+
         self.humanPoiAssignments = self.getPoiAssignments()
-        self.robotPoiAssignments = []
+        self.robotPoiAssignments = [] #from RL MODEL
+
         self.subscriber = self.create_subscription(DiverseArray, 'poiVisits', self.subCallback, 10)
+
         self.numParams = 6 #tBar, Fs, Ff, Fw, Pr, Correct(1)/Wrong(-1)        
         self.operatorMetrics = []
+        
+        #0: Safe POIs, 1: Threat POIs, 2: Humans, 3: UAVs, 4: UGVs
+        self.numUAVs = self.getNumAgents(3)
+        self.numUGVs = self.getNumAgents(4)
+        self.uavMetrics = []
+        self.ugvMetrics = []
+
         self.numPois = len(self.poiAttributes)
         self.poiVisitTimes = np.zeros((self.numPois, 1))
         self.currentTimes = []
         self.pictureTakingTime = 3 #seconds
         self.poisVisited = 0
+        
         self.imageQualities = ["low", "medium", "upper-medium", "high"]
         self.tBarLUT = {"low": [45, 125, 245], "medium": [35, 95, 195], "upper-medium": [25, 65, 145], "high": [15, 35, 95]}
         self.robotProbabilitiesLUT = {"low": [0.5, 0.3, 0.1], "medium": [0.6, 0.4, 0.2], "upper-medium": [0.7, 0.5, 0.3], "high": [0.8, 0.6, 0.4]}
 
         self.initializeNestedLists(self.operatorMetrics, len(self.humanAttributes), self.numParams)
+        self.initializeNestedLists(self.uavMetrics, self.numUAVs, 2)
+        self.initializeNestedLists(self.ugvMetrics, self.numUGVs, 2)
         self.initializeNestedLists(self.currentTimes, len(self.humanAttributes), 0)
 
     def getAgentAttributes(self, agentType, numAttributes):
@@ -100,6 +114,9 @@ class OperatorHub(Node):
 
         return humanPoiAssignments
 
+    def getNumAgents(self, i):
+        f = open(NUM_AGENTS_FILE, '+r')
+        return int(f.readline(i))
 
     def initializeNestedLists(self, array, outerParam, innerParam):
         for i in range(outerParam):
@@ -112,56 +129,66 @@ class OperatorHub(Node):
         robotName = msg.robot_name
         visitedPoiCoords = [round(msg.poi_x, 2), round(msg.poi_y, 2), round(msg.poi_z, 2)]
         arrivalTime = msg.arrival_time
+    
+        #Determine the poi difficulty level
+        poiDifficulty = -1
+        for i in range(len(self.poiAttributes)):
+            if self.poiAttributes[i][0] == visitedPoiCoords[0]:
+                poiDifficulty = self.poiAttributes[i][4]
+                self.poiVisitTimes[i][0] = arrivalTime #add arrival time to poi to "poi visit times" column vector
+                break
+        
+        #Determine how the robot got to the poi - robot means autonomous, operator means sharedMode
+        #If sharedMode then adjust image quality based on operator skill level in TAKING PICTURE
+        navigatingAgent = "robot" #or "operator"
+        if navigatingAgent == "robot":
+            imageQuality = self.getImageQuality(robotName, assignedOperator, sharedMode=False)
+        else:
+            assignedOperator = 0 #Determine which operator was assigned to drive the robot to this poi
+            imageQuality = self.getImageQuality(robotName, assignedOperator, sharedMode=True)
 
+        #Now work on determining which agent has been assigned to classify the poi
         #First try to find the robot assigned to this poi based on its xyz location
         foundAssignedRobot = False
         row = 0
         assignedRobot = -1
         for i, row in enumerate(self.robotPoiAssignments):
-            #self.get_logger().info(f"row: {row}")
             for coord in row:
                 if visitedPoiCoords[0] == coord[0] and visitedPoiCoords[1] == coord[1]:
                     assignedRobot = i
                     break
-        self.get_logger().info(f"AD assigned to robot {assignedOperator}")
         
+        #POI was actually assigned to a robot
+        if assignedRobot != -1:
+            self.get_logger().info(f"AD assigned to {assignedRobot}")
+            robotNumber = re.findall(r"\d+", assignedRobot)[0] #extract the number of the robot from string
+            robotClassificationAccuracy = self.robotProbabilitiesLUT[imageQuality][poiDifficulty - 1]
+            robotPredictionResult = self.flipFunction(robotClassificationAccuracy, poiDifficulty)
+            
+            if "moose" in assignedRobot:
+                self.mooseMetrics[robotNumber][0].append(robotClassificationAccuracy)
+                self.mooseMetrics[robotNumber][1].append(robotPredictionResult)
+            else:
+                self.mavicMetrics[robotNumber][0].append(robotClassificationAccuracy)
+                self.mavicMetrics[robotNumber][1].append(robotPredictionResult)
 
-        #If assignedRobot is -1 then that means the poi was not assigned to a robot -> instead it was assigned to a human
-        if assignedRobot == -1:
+        #POI was not assigned to a robot -> it was assigned to an operator instead
+        else:
             #Find the operator assigned to this poi based on its xyz location
             foundAssignedHuman = False
             row = 0
             assignedOperator = -1
             for i, row in enumerate(self.humanPoiAssignments):
-                #self.get_logger().info(f"row: {row}")
                 for coord in row:
                     if visitedPoiCoords[0] == coord[0] and visitedPoiCoords[1] == coord[1]:
                         assignedOperator = i
                         break
             self.get_logger().info(f"AD assigned to operator {assignedOperator}")
             
-            #Determine the poi difficulty and calculate t-bar value from the table II
-            #1. Determine the poi difficulty level
-            poiDifficulty = -1
-            for i in range(len(self.poiAttributes)):
-                if self.poiAttributes[i][0] == visitedPoiCoords[0]:
-                    poiDifficulty = self.poiAttributes[i][4]
-                    self.poiVisitTimes[i][0] = arrivalTime #add arrival time to poi to poi visit times column vector
-                    break
-
-            #2. Determine the base image quality based on the type of robot that took the image - UAV (medium) and UGV (upper-medium)
-            imageQuality = "upper-medium" if "moose" in robotName else "medium"
-
-            #3. Upgrade or downgrade the image quality by 1 based on the skill level of the human operator assigned
-            #Downgrade image quality if skill level is LOW
-            if self.humanAttributes[assignedOperator][4] > 0 and self.humanAttributes[assignedOperator][4] < m.pi / 12:
-                imageQuality = self.imageQualities[self.imageQualities.index(imageQuality) - 1]
+            #Adjust image quality one more time - this time to adjust/account for operator skill level in CLASSIFYING POI
+            imageQuality = self.getImageQuality(robotName, assignedOperator, sharedMode=True)
             
-            #Upgrade image quality if skill level is high
-            elif self.humanAttributes[assignedOperator][4] > m.pi / 6 and self.humanAttributes[assignedOperator][4] < m.pi / 4:
-                imageQuality = self.imageQualities[self.imageQualities.index(imageQuality) + 1]
-            
-            #4. Assign the t Bar value based on final image quality and poi difficulty
+            #Assign the t Bar value based on final image quality and poi difficulty
             tBar = self.tBarLUT[imageQuality][poiDifficulty - 1]
             self.operatorMetrics[assignedOperator][0].append(tBar)
             
@@ -216,21 +243,39 @@ class OperatorHub(Node):
             #Use flip function to convert probability into correct/wrong prediction
             binaryResult = self.flipFunction(predictionProbability, poiDifficulty)
             self.operatorMetrics[assignedOperator][5].append(binaryResult)
-            #self.get_logger().info(f"Oper metrics: {self.operatorMetrics}")
             if binaryResult >= 0:
                 self.get_logger().info(f"Operator {assignedOperator} identified AD correctly!")
             else:
                 self.get_logger().info(f"Operator {assignedOperator} identified AD incorrectly!")
 
             currentOperatorScore = round(np.mean(self.operatorMetrics[assignedOperator][5]), 2)
-            self.get_logger().info(f"Operator {assignedOperator} current score is {currentOperatorScore}")
+            self.get_logger().info(f"Operator {assignedOperator} current average score is {currentOperatorScore}")
 
         if self.poisVisited == len(self.poiAttributes):
             self.getSimScore()
 
+
+    def getImageQuality(self, robotName, assignedOperator, sharedMode):
+        #1. Determine the base image quality based on the type of robot that took the image - UAV (medium) and UGV (upper-medium)
+        imageQuality = "upper-medium" if "moose" in robotName else "medium"
+
+        if sharedMode == True:
+            #2. Upgrade or downgrade the image quality by 1 based on the skill level of the human operator assigned
+            #Downgrade image quality if skill level is LOW
+            if self.humanAttributes[assignedOperator][4] > 0 and self.humanAttributes[assignedOperator][4] < m.pi / 12:
+                imageQuality = self.imageQualities[self.imageQualities.index(imageQuality) - 1]
+                
+            #Upgrade image quality if skill level is high
+            elif self.humanAttributes[assignedOperator][4] > m.pi / 6 and self.humanAttributes[assignedOperator][4] < m.pi / 4:
+                imageQuality = self.imageQualities[self.imageQualities.index(imageQuality) + 1]
+        
+        return imageQuality
+
+
     def flipFunction(self, predictionProbability, poiDifficulty):
         return (poiDifficulty * 10) if random.uniform(0, 1) <= predictionProbability else (poiDifficulty * -10)
-    
+
+
     def getSimScore(self):
         operatorNetScores = np.zeros((1, len(self.humanAttributes))) 
         for i, metrics in enumerate(self.operatorMetrics):
