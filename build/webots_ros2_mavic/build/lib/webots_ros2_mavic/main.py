fixed moose stalling issue by adjusting precision tolerance

build/webots_ros2_mavic/build/lib/webots_ros2_mavic/main.py
@@ -15,38 +15,63 @@ POIS_FILE = '/home/arjun/SMART-LAB-ITAP-WEBOTS/webots_ros2_mavic/resource/pois'
 
 
 def main():
+    def findClosestPoint(centroid, pois, clusterIndexes, targetIndex):
+        minDistance = sys.maxsize
+        for i, poi in enumerate(pois):
+            if clusterIndexes[i] == targetIndex:
+                distance = m.sqrt((centroid[0] - poi[0]) ** 2 + (centroid[1] - poi[1]) ** 2)
+                if distance < minDistance:
+                    minDistance = distance
+                    closestPoi = poi
+        print(f"centroid: {centroid}   closest: {closestPoi}")
+        return closestPoi
+
+
     def performKMeans(numRobots, pois):
         kMeans = KMeans(n_clusters = numRobots)
         clusterIndexes = kMeans.fit_predict(pois)
+        #print(kMeans.cluster_centers_)
         clustersMatrix = [] #Each element is a list corresponding to a single cluster - the inner lists contain tuples of the xyz coordinates of pois in that cluster
         poisInCluster = []
         minDist = sys.maxsize
         clustersDict = {}
-
+        centroids = []
+        
         for i in range(len(clusterIndexes)):
             #If a member of the ith cluster has not been entered into the dict yet, check whether to append the centroid or the normal current point
             if clusterIndexes[i] not in clustersDict.keys():
-                centroid = tuple(kMeans.cluster_centers_[clusterIndexes[i]])
+                centroid = list(kMeans.cluster_centers_[clusterIndexes[i]])
                 if centroid in pois:
                     clustersDict[clusterIndexes[i]] = [centroid]
                 else:
-                    clustersDict[clusterIndexes[i]] = [pois[i]]
-            else:
+                    closestPoint = findClosestPoint(centroid, pois, clusterIndexes, clusterIndexes[i])
+                    clustersDict[clusterIndexes[i]] = [closestPoint]
+                
+                centroids.append(clustersDict[clusterIndexes[i]][0])
+
+            if pois[i] not in centroids:
                 clustersDict[clusterIndexes[i]].append(pois[i])
         
+        #print(f"DICT: {clustersDict}")
+        
         #For each key in the dict, sort the values list based on their closeness to the centroid
+        sum = 0
         for cluster in clustersDict.keys():
             poiList = clustersDict[cluster]
+            sum += len(poiList)
             if len(poiList) > 0:
-                poisSorted = sorted(poiList, key=lambda d: m.dist(d, kMeans.cluster_centers_[cluster]))
+                poisSorted = sorted(poiList, key=lambda d: m.dist(d, poiList[0]))
                 clustersMatrix.append(poisSorted)
 
+        print(sum)
         count = 0
         for r in clustersMatrix:
             for point in r:
                 if point not in pois:
                     count += 1
 
+        for r in clustersMatrix:
+            print(r)
         return clustersMatrix
 
     def RL():
@@ -88,7 +113,7 @@ def main():
     numUAVs = 3
     numUGVs = 4
     numRobots = numUAVs + numUGVs
-    uavHeight = 19
+    uavHeight = 7
 
     writeToFile([numSafe, numThreats, numHumans, numUAVs, numUGVs], SIM_AGENT_NUM_FILE)
 
@@ -96,8 +121,14 @@ def main():
     pois = itapSim.getPOIs()
     poisFlattened = list(np.concatenate(pois).flat)
     writeToFile(poisFlattened, POIS_FILE)
-    clustersMatrix = performKMeans(numRobots, pois)
+    
+    #Normal line of code
+    clustersMatrix = performKMeans(numRobots, pois) 
     writeWaypoints(clustersMatrix, numUGVs, numUAVs, uavHeight)
+    
+    #Hard-coded line for demo
+    
+
 
     humanAttributes = itapSim.getHumanAttributes()
     humanAttrFlattened = list(np.concatenate(humanAttributes).flat)

webots_ros2_mavic/webots_ros2_mavic/main.py
@@ -113,7 +113,7 @@ def main():
     numUAVs = 3
     numUGVs = 4
     numRobots = numUAVs + numUGVs
-    uavHeight = 19
+    uavHeight = 7
 
     writeToFile([numSafe, numThreats, numHumans, numUAVs, numUGVs], SIM_AGENT_NUM_FILE)
 
@@ -121,8 +121,14 @@ def main():
     pois = itapSim.getPOIs()
     poisFlattened = list(np.concatenate(pois).flat)
     writeToFile(poisFlattened, POIS_FILE)
-    clustersMatrix = performKMeans(numRobots, pois)
+    
+    #Normal line of code
+    clustersMatrix = performKMeans(numRobots, pois) 
     writeWaypoints(clustersMatrix, numUGVs, numUAVs, uavHeight)
+    
+    #Hard-coded line for demo
+    
+
 
     humanAttributes = itapSim.getHumanAttributes()
     humanAttrFlattened = list(np.concatenate(humanAttributes).flat)